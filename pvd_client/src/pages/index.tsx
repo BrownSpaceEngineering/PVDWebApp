788abf5fdda9d859c35a3b4c2b6ab85d2610e169
Hailey <hailey_coval@brown.edu>
2024-10-08 14:37:02 -0400

    very problematic block code

--- pvd_client/src/pages/index.tsx
+++ pvd_client/src/pages/index.tsx
@@ -2,7 +2,12 @@
 
 import React, { useEffect, useState } from 'react'
 
+// import * as Blockly from 'blockly';
+
 function index() {
+  // Passes the ID.
+  // const workspace = Blockly.inject(document.getElementById('blocklyDiv'), { /* config */ });
+        
 
   const [message, setMessage] = useState("loading...")
 
@@ -16,9 +21,56 @@ function index() {
           }
         )
     },[])
-    
-    const [windowWidth, setWindowWidth] = useState(0);
 
+        // Require Blockly core.
+    const Blockly = require('blockly/core');
+    // Require the default blocks.
+    const libraryBlocks = require('blockly/blocks');
+    // Require a generator.
+    const {javascriptGenerator} =  require('blockly/javascript');
+    // Require a message file.
+    const En = require('blockly/msg/en');
+    Blockly.setLocale(En);
+
+    const [windowWidth, setWindowWidth] = useState(0);
+    const toolbox = {
+      "kind": "flyoutToolbox",
+      "contents": [
+        {
+          "kind": "block",
+          "type": "controls_if"
+        },
+        {
+          "kind": "block",
+          "type": "controls_repeat_ext"
+        },
+        {
+          "kind": "block",
+          "type": "logic_compare"
+        },
+        {
+          "kind": "block",
+          "type": "math_number"
+        },
+        {
+          "kind": "block",
+          "type": "math_arithmetic"
+        },
+        {
+          "kind": "block",
+          "type": "text"
+        },
+        {
+          "kind": "block",
+          "type": "text_print"
+        },
+      ]
+    }
+    useEffect(() => {
+      if (typeof window !== 'undefined') {
+        const workspace = Blockly.inject('blocklyDiv', { toolbox: toolbox });
+      }
+    }, []);
     useEffect(() => {
         setWindowWidth(window.innerWidth);
         const handleResize = () => setWindowWidth(window.innerWidth);
@@ -27,35 +79,36 @@ function index() {
     }, []);
 
     return (
-      <div style={{ display: 'flex', flexDirection: windowWidth < 1200 ? 'column' : 'row' }}>
-        <div style={{ flex: 1, padding: '10px', border: '0px solid black' }}>
-            <section className="controls">
-            <div className="tabs">
-                <button className="tab active">Control</button>
-                <button className="tab">Move</button>
-                <button className="tab">Draw</button>
-            </div>
-            <div className="block-area">
-                <div className="block"></div>
-                <div className="block"></div>
-                <div className="block"></div>
-            </div>
-            </section>
-        </div>
-        <div style={{ flex: 1, padding: '10px', border: '0px solid black' }}>
-        <section className="display-area">
-            <div className="screen"></div>
-            <div className="buttons">
-                <button className="btn stop"></button>
-                <button className="btn play"></button>
-                <button className="btn circle"></button>
-            </div>
-        </section>
-        </div>
-      </div>
-        
-        
+      
+      // <div style={{ display: 'flex', flexDirection: windowWidth < 1200 ? 'column' : 'row' }}>
+      //   <div style={{ flex: 1, padding: '10px', border: '0px solid black' }}>
+      //       <section className="controls">
+      //       <div className="tabs">
+      //           <button className="tab active">Control</button>
+      //           <button className="tab">Move</button>
+      //           <button className="tab">Draw</button>
+      //       </div>
+      //       <div className="block-area">
+      //           <div className="block"></div>
+      //           <div className="block"></div>
+      //           <div className="block"></div>
+      //       </div>
+      //       </section>
+      //   </div>
+      //   <div style={{ flex: 1, padding: '10px', border: '0px solid black' }}>
+      //   <section className="display-area">
+      //       <div className="screen"></div>
+      //       <div className="buttons">
+      //           <button className="btn stop"></button>
+      //           <button className="btn play"></button>
+      //           <button className="btn circle"></button>
+      //       </div>
+      //   </section>
+      //   </div>
+      // </div>
+      <div id="blocklyDiv" style={{ height: '480px', width: '600px', backgroundColor: "#fff"}}></div>
     )
+    
 }
 
 export default index
